3cbf64dab5daabe36e5d28c8768b802b9a882f10
Cristian Morales <cmoralesavendano6@gmail.com>
2026-03-26 00:11:13 -0500

    update-menu

--- v3/app.py
+++ v3/app.py
@@ -7,11 +7,16 @@ def menu():
     while isMenu :
         try:
             menu = int(input("""
-            Please enter a digit number (1-4) :
-            1. Add product
-            2. Show product
-            3. Calculate statistics
-            4. Exit
+            Please enter a digit number (1-9) :
+            1. Create product
+            2. Show Inventory
+            3. Search Product
+            4. Update Product
+            5. Delete Product
+            6. Statistics
+            7. Upload CSV
+            8. Charge CSV
+            9. Exit
             Enter →  """))
             validationNum(menu)
             if menu == 1:
@@ -19,8 +24,18 @@ def menu():
             elif menu == 2:
                 showInventory()
             elif menu == 3:
-                calculateStatistics()
+                print("Here you can search product")
             elif menu == 4:
+                print("Here you can update product")
+            elif menu == 5:
+                print("Here you can delete product")
+            elif menu == 6:
+                calculateStatistics()
+            elif menu == 7:
+                print("Here you can Upload CSV")
+            elif menu == 8:
+                print("Here you can charge CSV local")
+            elif menu == 9:
                 isMenu = False
             else:
                 print("")
